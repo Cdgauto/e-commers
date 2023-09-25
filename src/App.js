0b07ae2b8e3f94bf492f8b5638cfe036295bf081
Cristian Gauto <cristiangauto299@gmail.com>
2023-09-25 09:01:10 -0300

    v2. contador con boton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from './components/NavBar/NavBar';
 import CartWidget from './components/CartWidget/CartWidget';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ItemCount from './components/ItemCount/ItemCount';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App"> 
       <NavBar />
       <ItemListContainer greeting={'Hola a todos'}/>
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agregada ", quantity)}/>
     </div>
   );
 };
